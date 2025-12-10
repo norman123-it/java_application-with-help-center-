579a76b9ef07c3377e01d3a34a07aee9acc53671
norman123-it <narvaeznorman3@gmail.com>
2025-12-10 04:41:03 -0800

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/DBConnection.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RM
+ */
+public class DBConnection {
+    
+    
+}
